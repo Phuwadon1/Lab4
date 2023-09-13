09343dcd9d2d00857396bfcee6b35e7b754b1466
Phuwadon <phuwadon.s@ku.th>
2023-09-13 11:57:19 +0700

    master

--- student_list.php
+++ student_list.php
@@ -4,10 +4,13 @@ $sql = "SELECT * FROM student";
 $result = mysqli_query($conn, $sql);
 echo "<center>";
 echo "<table border=1 width=40%>";
-echo "<tr><th>Student Code</th><th>Student Name</th></tr>";
+echo "<tr><th>Student Code</th><th>Student Name</th><th>Gender</th></tr>";
 while($row = mysqli_fetch_assoc($result)){
-    echo "<tr><td>".$row["student_code"]."</th><td>".$row["student_name"]."</td></tr>";
+    echo "<tr><td>".$row["student_code"]."</td><td>"
+    .$row["student_name"]."</td><td>"
+    .$row["gender"]."</td></tr>";
 }
-echo "</table>";
+echo "</table><br>";
+echo "<a href=add_student.php>Add Student</a>";
 echo "</center>";
 ?>
